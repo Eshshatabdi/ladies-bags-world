8cb87acac705d56df16b93577a774da71cfce158
Esh shatabdi <eshshatabdi@gmail.com>
2022-03-25 16:55:40 -0400

    handler set in product

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,80 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import image1 from '../../michel cors.jpg';
-import image2 from '../../ck.jpg';
-import image3 from '../../garage2.jpg';
-import image4 from '../../koras.jpg';
-import image5 from '../../kelvin clain.jpg';
-import image6 from '../../Dkny.jpg';
-import image7 from '../../markjeckob.jpg';
-import image8 from '../../prada.jpg';
-import image9 from '../../prada.jpg';
 import Card from '../Card/Card';
 
 
 
 const Product = () => {
     const [products, setProducts] = useState([]);
+
+    const [carts, setCarts] = useState([]);
+
+
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
-    // const products = [
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'michel cors',
-    //         price: '4000',
-    //         image: { image1 }
-    //     },
-    // ]
+
+    const handleAddToCart = (product) => {
+        const newCart = [...carts, product];
+        setCarts(newCart);
+
+    }
+
     return (
         <div>
             <h2 className='text-center color-info'>Ladis Bags</h2>
@@ -84,7 +30,8 @@ const Product = () => {
                     {
                         products.map(product => <Card
                             key={product.id}
-                            product={product}></Card>)
+                            product={product}
+                            handleAddToCart={handleAddToCart}></Card>)
 
                     }
 
