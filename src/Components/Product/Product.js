1d5192c114ecf7e9d77e4ced03e2840f0ce310ed
Esh shatabdi <eshshatabdi@gmail.com>
2022-03-26 11:02:06 -0400

    choose buttom work

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -34,7 +34,8 @@ const Product = () => {
         let random = carts[Math.floor(Math.random() * carts.length)]
 
         setItem(random);
-        console.log(random)
+        setCarts([])
+
 
 
     }
@@ -48,50 +49,50 @@ const Product = () => {
         <div>
             <h2 className='text-center text-primary'>Ladies Bags World</h2>
 
-            <div>
-                <div className='bg-light'>
-                    <h4>Choose your item</h4>
+            {/* <div> */}
+            <div className='bg-light ps-3'>
+                <h4>Choose your item</h4>
 
-                    {
+                {
 
-                        carts.map(cart => <Cart key={cart.id} cart={cart}></Cart>)
-                    }
-                    <div>
-                        <h4>{item.name}
-                        </h4>
-                    </div>
-
-                    <div>
-                        <button onClick={() => { chooseItems(carts) }} className='bg-primary text-white border-0 radius'>Choose product</button>
-                        <br />
-                        <button onClick={removeCart} className='mt-3 bg-primary text-white border-0 radius'>Choose Again</button>
-
-
-                    </div>
-
-
+                    carts.map(cart => <Cart key={cart.id} cart={cart}></Cart>)
+                }
+                <div>
+                    <h4>{item.name}
+                    </h4>
                 </div>
 
+                <div>
+                    <button onClick={() => { chooseItems(carts) }} className='bg-primary text-white border-0 radius'>Choose product</button>
+                    <br />
+                    <button onClick={removeCart} className='mt-3 bg-primary text-white border-0 radius'>Choose Again</button>
 
-
-
-
-
-
-
-                <div className="row container">
-                    {
-                        products.map(product => <Card
-                            key={product.id}
-                            product={product}
-                            handleAddToCart={handleAddToCart}></Card>)
-
-                    }
 
                 </div>
 
 
             </div>
+
+
+
+
+
+
+
+
+            <div className=" row container ">
+                {
+                    products.map(product => <Card
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}></Card>)
+
+                }
+
+            </div>
+
+
+            {/* </div> */}
 
             <div className=' row container'>
                 <Qusans></Qusans>
